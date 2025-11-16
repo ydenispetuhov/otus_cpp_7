475339501d4886caa53a721703be554e4d5c2177
ydenispetuhov <ydenispetuhov@yandex.ru>
2025-11-17 00:02:45 +0700

    start project

--- src/CommandProcessor.cpp
+++ src/CommandProcessor.cpp
@@ -5,6 +5,9 @@
 #include <iostream>
 #include <queue>
 #include <stack>
+#include <algorithm>
+#include <cctype>
+#include <locale>
 #include "processor.h"
 #include "reader.h"
 
@@ -17,35 +20,24 @@ public:
     void process_data() override {
         std::string line_;
         while (reader_->read(line_)) {
+            ltrim(line_);
             if (line_ == "{") {
-                if (stack.empty()) {
-                    while (!queue.empty()) {
-                        writer_->write(queue.front());
-                        queue.pop();
-                    }
-                    writer_->write("\n");
+                if (stack.empty() && !queue.empty()) {
+                    prepare_command_and_write(queue, writer_);
                 }
                 stack.push(line_);
                 continue;
             } else if (line_ == "}") {
                 stack.pop();
                 if (stack.empty()) {
-                    while (!queue.empty()) {
-                        writer_->write(queue.front());
-                        queue.pop();
-                    }
-                    writer_->write("\n");
+                    prepare_command_and_write(queue, writer_);
                 }
                 continue;
             } else {
                 queue.push(line_);
             }
             if (stack.empty() && queue.size() == c_block_size) {
-                while (!queue.empty()) {
-                    writer_->write(queue.front());
-                    queue.pop();
-                }
-                writer_->write("\n");
+                prepare_command_and_write(queue, writer_);
             }
         }
     };
@@ -53,6 +45,35 @@ public:
     ~CommandProcessor() = default;
 
 private:
+
+    void prepare_command_and_write(std::queue<std::string>& queue, std::shared_ptr<IWriter>& writer) {
+        // std::string result = std::reduce(queue.begin(), queue.end(), " ");
+        // result += "\n";
+        // writer_->write(result);
+        std::string result;
+        while (!queue.empty()) {
+                    result += queue.front();
+                    result += " ";
+                    queue.pop();
+                }
+                result += "\n";
+                writer_->write(result);
+    }
+
+    // Trim from the start (in place)
+    static void ltrim(std::string &s) {
+        s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch) {
+            return !std::isspace(ch);
+        }));
+    }
+
+    // Trim from the end (in place)
+    static void rtrim(std::string &s) {
+        s.erase(std::find_if(s.rbegin(), s.rend(), [](unsigned char ch) {
+            return !std::isspace(ch);
+        }).base(), s.end());
+    }
+
     std::shared_ptr<IWriter> writer_;
     std::shared_ptr<IReader> reader_;
     std::queue<std::string> queue;
