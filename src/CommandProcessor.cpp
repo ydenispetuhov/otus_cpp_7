908be82da28ac40aefa580b415d2e32f2a804260
ydenispetuhov <ydenispetuhov@yandex.ru>
2025-11-16 13:56:45 +0700

    start project

--- src/CommandProcessor.cpp
+++ src/CommandProcessor.cpp
@@ -3,18 +3,50 @@
 //
 
 #include <iostream>
+#include <queue>
+#include <stack>
 #include "processor.h"
 #include "reader.h"
 
 class CommandProcessor : public IProcessor {
 public:
-    CommandProcessor(std::shared_ptr<IReader> reader, std::shared_ptr<IWriter> writer) : reader_{reader},
-                                                                                         writer_{writer} {}
+    CommandProcessor(int block_size, std::shared_ptr<IReader> reader, std::shared_ptr<IWriter> writer) :    c_block_size{block_size},
+                                                                                                            reader_{reader},
+                                                                                                            writer_{writer} {}
 
     void process_data() override {
         std::string line_;
         while (reader_->read(line_)) {
-            writer_->write(line_);
+            if (line_ == "{") {
+                if (stack.empty()) {
+                    while (!queue.empty()) {
+                        writer_->write(queue.front());
+                        queue.pop();
+                    }
+                    writer_->write("\n");
+                }
+                stack.push(line_);
+                continue;
+            } else if (line_ == "}") {
+                stack.pop();
+                if (stack.empty()) {
+                    while (!queue.empty()) {
+                        writer_->write(queue.front());
+                        queue.pop();
+                    }
+                    writer_->write("\n");
+                }
+                continue;
+            } else {
+                queue.push(line_);
+            }
+            if (stack.empty() && queue.size() == c_block_size) {
+                while (!queue.empty()) {
+                    writer_->write(queue.front());
+                    queue.pop();
+                }
+                writer_->write("\n");
+            }
         }
     };
 
@@ -23,6 +55,9 @@ public:
 private:
     std::shared_ptr<IWriter> writer_;
     std::shared_ptr<IReader> reader_;
+    std::queue<std::string> queue;
+    std::stack<std::string> stack;
+    int c_block_size;
 };
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -6,15 +6,17 @@
 #include <memory>
 
 int main(int argc, char *argv[]) {
-    std::ifstream in("../bulk_origin.txt");
+    // std::ifstream in("../bulk_origin.txt");
     std::ofstream of(
             "bulk" + std::to_string(std::chrono::duration_cast<std::chrono::nanoseconds>(
                     std::chrono::high_resolution_clock::now().time_since_epoch()).count()) + ".log", std::ios::app);
-    if (in.is_open()) {
-        std::shared_ptr<ConsoleReader> consoleReader = std::make_shared<ConsoleReader>(ConsoleReader(in));
+    int block_size = std::stoi(argv[1]);
+    // if (in.is_open()) {
+        // std::shared_ptr<ConsoleReader> consoleReader = std::make_shared<ConsoleReader>(ConsoleReader(in));
+        std::shared_ptr<ConsoleReader> consoleReader = std::make_shared<ConsoleReader>(ConsoleReader());
         std::shared_ptr<FileWriter> fileWriter = std::make_shared<FileWriter>(FileWriter(of));
-        CommandProcessor commandProcessor = CommandProcessor(consoleReader, fileWriter);
+        CommandProcessor commandProcessor = CommandProcessor(block_size, consoleReader, fileWriter);
         commandProcessor.process_data();
-    }
-    in.close();
+    // }
+    // in.close();
 }
