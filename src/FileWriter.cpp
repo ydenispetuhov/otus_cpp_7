0c00ce6825b916901a3892ccd1325b45923a45d1
ydenispetuhov <ydenispetuhov@yandex.ru>
2025-11-16 23:11:52 +0700

    start project

--- src/FileWriter.cpp
+++ src/FileWriter.cpp
@@ -8,31 +8,25 @@
 
 class FileWriter : public IWriter {
 public:
-    explicit FileWriter(std::ofstream& file_) : m_file(file_) {}
-//    explicit FileWriter(const std::string filename_prefix, const std::string filename_suffix) : m_file(std::ofstream(
-//                    filename_prefix + std::to_string(std::chrono::duration_cast<std::chrono::nanoseconds>(
-//                            std::chrono::high_resolution_clock::now().time_since_epoch()).count()) + filename_suffix, std::ios::app)) {}
 
-    FileWriter(FileWriter &&fileWriter) : m_file(fileWriter.getFileStream()) {}
-
-
-    FileWriter(FileWriter &fileWriter) : m_file(fileWriter.getFileStream()) {}
+    explicit FileWriter(const std::string filename_prefix_, const std::string filename_suffix_) : filename_prefix(filename_prefix_), filename_suffix(filename_suffix_) {}
 
     void write(const std::string &str) override {
-        m_file << str;
-    }
-
-    void write(const std::string &str, std::uint64_t time) {
-        m_file << str;
-    }
-
-    std::ofstream &getFileStream() {
-        return m_file;
+        makeofstream() << str;
     }
 
     virtual ~FileWriter() = default;
 
 private:
-    std::ofstream& m_file;
+
+    std::ofstream makeofstream(){
+        std::ofstream of(
+        filename_prefix + std::to_string(std::chrono::duration_cast<std::chrono::nanoseconds>(
+                        std::chrono::high_resolution_clock::now().time_since_epoch()).count()) + filename_suffix, std::ios::app);
+        return of;
+    }
+
+    std::string filename_prefix;
+    std::string filename_suffix;
 };
 
--- src/main.cpp
+++ src/main.cpp
@@ -6,17 +6,9 @@
 #include <memory>
 
 int main(int argc, char *argv[]) {
-    // std::ifstream in("../bulk_origin.txt");
-    std::ofstream of(
-            "bulk" + std::to_string(std::chrono::duration_cast<std::chrono::nanoseconds>(
-                    std::chrono::high_resolution_clock::now().time_since_epoch()).count()) + ".log", std::ios::app);
     int block_size = std::stoi(argv[1]);
-    // if (in.is_open()) {
-        // std::shared_ptr<ConsoleReader> consoleReader = std::make_shared<ConsoleReader>(ConsoleReader(in));
-        std::shared_ptr<ConsoleReader> consoleReader = std::make_shared<ConsoleReader>(ConsoleReader());
-        std::shared_ptr<FileWriter> fileWriter = std::make_shared<FileWriter>(FileWriter(of));
-        CommandProcessor commandProcessor = CommandProcessor(block_size, consoleReader, fileWriter);
-        commandProcessor.process_data();
-    // }
-    // in.close();
+    std::shared_ptr<ConsoleReader> consoleReader = std::make_shared<ConsoleReader>(ConsoleReader());
+    std::shared_ptr<FileWriter> fileWriter = std::make_shared<FileWriter>(FileWriter("bulk", ".log"));
+    CommandProcessor commandProcessor = CommandProcessor(block_size, consoleReader, fileWriter);
+    commandProcessor.process_data();
 }
